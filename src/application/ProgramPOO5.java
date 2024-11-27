3fa97d787daaa2e5dc18d62199e9148dc5845897
Lucas Paim <lucaspaimrj21@gmail.com>
2024-11-27 19:57:14 -0300

    mais exercícios

---
+++ src/application/ProgramPOO5.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Student;
+
+public class ProgramPOO5 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Student student = new Student();
+		
+		System.out.print("Name: ");
+		student.name = sc.nextLine();
+		System.out.print("First Grade: ");
+		student.firstGrade = sc.nextDouble();
+		System.out.print("Second Grade: ");
+		student.secondGrade = sc.nextDouble();
+		System.out.print("Third Grade: ");
+		student.thirdGrade = sc.nextDouble();
+		
+		System.out.println("FINAL GRADE: " + student.finalGrade());
+		
+		if (student.finalGrade() >= 60) {
+			System.out.print("PASS");
+		}
+		else {
+			System.out.print("FAILED" + "\n");
+			System.out.print("MISSING " + student.missingPoints() + " POINTS");
+		}
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/entities/Student.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class Student {
+	public String name;
+	public double firstGrade;
+	public double secondGrade;
+	public double thirdGrade;
+	
+	public double finalGrade() {
+		return firstGrade + secondGrade + thirdGrade;
+	}
+	
+	public double missingPoints() {
+		if (finalGrade() < 60.0) {
+			return 60.0 - finalGrade();
+		}
+		else {
+			return 0.0;
+		}
+	}
+}
+	
+
+
